fbd7f6fc57fd8e113fb5e9f0ff697ca788d29d94
Joe Wilson <joe@volaresystems.com>
2020-06-01 18:25:49 -0600

    SharePoint Web Part for listing the current user's claims

---
+++ ClaimsViewerWebPart/ClaimsViewerWebPart/ClaimsViewerWebPart.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+
+namespace ClaimsViewerWebPart.ClaimsViewerWebPart
+{
+    [ToolboxItemAttribute(false)]
+    public class ClaimsViewerWebPart : WebPart
+    {
+        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+        private const string _ascxPath = @"~/_CONTROLTEMPLATES/ClaimsViewerWebPart/ClaimsViewerWebPart/ClaimsViewerWebPartUserControl.ascx";
+
+        protected override void CreateChildControls()
+        {
+            var control = Page.LoadControl(_ascxPath);
+            Controls.Add(control);
+        }
+    }
+}
---
+++ ClaimsViewerWebPart/ClaimsViewerWebPart/ClaimsViewerWebPartUserControl.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+using Microsoft.IdentityModel.Claims;
+
+namespace ClaimsViewerWebPart.ClaimsViewerWebPart
+{
+    public partial class ClaimsViewerWebPartUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var claimsPrincipal = (IClaimsPrincipal) Page.User;
+            var claimsIdentity = (IClaimsIdentity) claimsPrincipal.Identity;
+
+            claimsGrid.DataSource = claimsIdentity.Claims;
+            Page.DataBind();
+        }
+    }
+}
